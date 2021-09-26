f38fb55c95711bde33fd144e43fb2863f79ea9c2
Angelo Geulin <angelogeulin123@gmail.com>
2021-09-27 06:38:08 +0800

    index.js: allow cross-origin requests

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express();
 // Handle JSON bodies.
 app.use(express.json());
 
+// Allow Cross-Origin requests (CORS).
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Products route.
 const products_route = require('./routes/products')
 app.use('/products', products_route);
